7d3a5234794dfe05774228dfec8d616dc110d793
Michael Zampogna <mrz@jetscholar.com>
2023-08-31 10:33:15 +1000

    node env

---
+++ server.js
@@ -0,0 +1,36 @@
+// if (process.env.NODE_ENV !== 'production') {
+//     require('dotenv').config()
+// }
+
+const port = 6900
+const express = require('express')
+const expressLayouts = require('express-ejs-layouts')
+const app = express()
+
+
+// set and use things
+app.set('view engine', 'ejs')
+app.set('views', __dirname + '/views')
+app.set('layout', 'layouts/layout')
+app.use(expressLayouts)
+app.use(express.static('public'))
+app.use(express.json({ limit: '1mb' }))
+
+// Routes
+
+// Basic route
+app.get('/', (req, res) => {
+    res.send('Hello World')
+})
+
+// const indexRouter = require('./routes/index')
+
+// app.use('/', indexRouter)
+
+// app.listen(process.env.PORT || port, () => {
+//     console.log('Web App ready at http://localhost:' + port)
+// })
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`)
+})
